62dea7857d5237c7eb575fa5cc4307fe95f2f862
Sondos Ahmed Ibrahim <sunnysondos42@gmail.com>
2022-10-05 10:54:33 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names, searchForName){
+    return names.filter(driver => searchForName.toLowerCase() === driver.toLowerCase())
+  }
+  
+  function fuzzyMatch(names, searchForStr){
+    return names.filter(driver => driver.startsWith(searchForStr))
+  }
+  
+  function matchName(names, nameOfStr){
+    return names.filter(driver => driver.name === nameOfStr)
+  } 
